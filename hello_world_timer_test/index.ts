eaa666ee0266200ff131d06590525bb0486ac180
FraserMuir <frasermuir14@gmail.com>
2023-01-26 15:20:20 +0000

    fix tests

--- hello_world_timer_test/index.ts
+++ hello_world_timer_test/index.ts
@@ -1,13 +1,13 @@
 import type { AzureFunction, Context, Timer, HttpResponse } from "@azure/functions";
 import { delay } from "../src/utils";
 
-const timerTrigger: AzureFunction = async (context: Context, req: Timer): Promise<HttpResponse> => {
+const timerTrigger: AzureFunction = async (context: Context, timer: Timer): Promise<HttpResponse> => {
   context.log("Hello from timer!");
 
   await delay(1000);
 
   return {
-    body: `Hello World! Timer trigger will next trigger at ${req.scheduleStatus.next || "Unknown"}`,
+    body: `Hello World! Timer trigger will next trigger at ${timer.scheduleStatus.next || "Unknown"}`,
   };
 };
 
